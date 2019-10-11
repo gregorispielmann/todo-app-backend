d262023db0c844fdf1ef520112dbc2a03119a961
Gregori Spielmann <gregorispielmann@MacBook-Pro-de-Gregori.local>
2019-10-11 08:19:39 -0300

    correction pm2

--- src/config/server.js
+++ src/config/server.js
@@ -1,17 +1,15 @@
-const port = 3003
-const bodyParser = require('body-parser')
-const express = require('express')
-const server = express()
-const allowCors = require('./cors')
+const port = process.env.PORT || 8080;
+const bodyParser = require("body-parser");
+const express = require("express");
+const server = express();
+const allowCors = require("./cors");
 
-server.use(bodyParser.urlencoded({ extended: true }))
-server.use(bodyParser.json())
-server.use(allowCors)
+server.use(bodyParser.urlencoded({ extended: true }));
+server.use(bodyParser.json());
+server.use(allowCors);
 
-server.listen(
-    port, function () {
-        console.log(`Backend rodando na porta ${port}`)
-    }
-)
+server.listen(port, function() {
+  console.log(`Backend rodando na porta ${port}`);
+});
 
-module.exports = server
+module.exports = server;
