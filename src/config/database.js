9502fbb82630bd5d7618d0b7831695eab33e161b
Gregori Spielmann <gregorispielmann@MacBook-Pro-de-Gregori.local>
2019-10-11 08:34:13 -0300

    correction pm2

--- src/config/database.js
+++ src/config/database.js
@@ -5,7 +5,7 @@ mongoose.Promise = global.Promise;
 
 // module.exports = mongoose.connect("mongodb://localhost:3100/todos", { useUnifiedTopology: true });
 module.exports = mongoose.connect(
-  "mongodb+srv://tester:tester123@gregorispielmann-kkcp0.mongodb.net/todo_app?retryWrites=true&w=majority",
+  "mongodb+srv://todo_app:todoapp@gregorispielmann-kkcp0.mongodb.net/todo_app?retryWrites=true&w=majority",
   {
     useNewUrlParser: true,
     useUnifiedTopology: true
