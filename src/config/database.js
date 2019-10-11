ec2224f94112f152e2d612675342ebaf263453b0
Gregori Spielmann <gregorispielmann@MacBook-Pro-de-Gregori.local>
2019-10-11 08:09:11 -0300

    correction pm2

--- src/config/database.js
+++ src/config/database.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 // remove warning from mongoose promise (deprecated)
 mongoose.Promise = global.Promise;
 
-// module.exports = mongoose.connect('mongodb://localhost/todo')
+// module.exports = mongoose.connect("mongodb://localhost:3100/todos", { useUnifiedTopology: true });
 module.exports = mongoose.connect(
   "mongodb+srv://tester:tester123@gregorispielmann-kkcp0.mongodb.net/todo_app?retryWrites=true&w=majority",
   {
